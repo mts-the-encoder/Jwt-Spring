966b67a16f5d99405a4d5cec27d52d84fe760761
mts-the-encoder <matheus.nsantos@bandtec.com.br>
2023-03-10 22:32:12 -0300

    project structure... funny

--- back-end/src/main/java/sptech/school/backend/comunication/response/AuthenticationResponse.java
+++ back-end/src/main/java/sptech/school/backend/comunication/response/AuthenticationResponse.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import sptech.school.backend.entities.enums.Role;
 
 @Data
 @Builder
@@ -12,4 +13,5 @@ import lombok.NoArgsConstructor;
 public class AuthenticationResponse {
 
   private String token;
+  private Role role;
 }
